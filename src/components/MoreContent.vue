3e2911bd84493dda24eb20093bff16ef700c7fbc
QingMu39-Gao <QingMu39-Gao@users.noreply.github.com>
2026-09-05 21:47:08 +0800

    站点个性化：清沐的个人博客（头像/简介/社交与网站链接/网易云歌单/自由内容区）

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    <div class="welcome">
+      <p class="hi">你好，我是清沐 ✨</p>
+      <p class="sub">初音未来 &amp; VOCALOID 爱好者，欢迎来到我的小站。</p>
+    </div>
+  </div>
 </template>
 
+<!-- 此处内容可自由修改：公告、语录、计数器、小插件等 -->
 <style lang="scss" scoped>
 .more-content {
   display: flex;
@@ -10,5 +16,19 @@
   margin-top: 20px;
   width: 100%;
   height: 100%;
+
+  .welcome {
+    text-align: center;
+    color: #fff;
+    .hi {
+      font-size: 1.2rem;
+      margin-bottom: 0.6rem;
+      text-shadow: 0 0 5px #00000060;
+    }
+    .sub {
+      font-size: 0.9rem;
+      opacity: 0.8;
+    }
+  }
 }
 </style>
